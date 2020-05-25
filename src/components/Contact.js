b481340b3e06da716ce2ff0edb3c74c3b7f65d64
Anna Dąbrowska <anna@onet.pl>
2020-05-25 14:32:07 +0200

    next commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,130 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+// importuję react-modal
+import Modal from "react-modal";
+import axios from "axios";
 
 // funkcjonalny komponent, bo nie używamy stanów
 
-const Contact= (props)=> {
-     console.log(props)
-    // setTimeout(()=> {
-    //     props.history.push('/about')
-    // }, 2000);
-    return (
-        <div className="container">
-            <h4 className="center">Contact</h4>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+Modal.setAppElement("#root");
 
+// W klasowym komponencie to działało, trzeba zamienić na funkcyjny, ze względu na hooki
+// componentDidMount() {
+//   axios
+//     .get("https://type.fit/api/quotes") //pobrać get
+//     .then((res) => {
+//       console.log(res);
+//       this.setState({
+//         quotes: res.data.Math.random(),
+//       });
+//     });
+// }
 
+// const Contact= ()=> czy function Contact(){}  powinno być?
+const Contact = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [quote, setQuote] = useState(["aaa"]);
+  const [quoteAuthor, setQuoteAuthor] = useState(["xxxx"]);
+  // const quoteArray = [];
+
+  // async function modalAxios() {
+  //   axios.get("https://type.fit/api/quotes").then((res) => {
+  //     console.log(res.data);
+  //     setModalIsOpen(res.data.Math.random());
+  //     console.log(modalIsOpen);
+  //   });
+  // }
+
+  // useEffect wykorzytujemy w Hooks
+
+  async function fetchData() {
+    const res = await fetch("https://type.fit/api/quotes");
+    console.log(res);
+
+    // const todoList = todos.length
+    res.json().then((res) => {
+      const randomNumber = Math.floor(Math.random() * res.length);
+      console.log(res[randomNumber].text);
+      console.log(res[randomNumber].author);
+
+      console.log(randomNumber);
+      setQuote(res[randomNumber].text);
+      setQuoteAuthor(res[randomNumber].author);
+      console.log(quoteAuthor);
+      console.log(res[randomNumber].author);
+      // setModalIsOpen(res);
+    });
+    console.log(typeof res);
+  }
+
+  // function randomize(myArray) {
+  //   const myArray = [1 - 50];
+  //   randint = Math.floor(Math.random() * myArray.length);
+  //   return randint;
+  //   console.log(myArray);
+  // }
+  // na początek
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  //   useEffect(async () => {
+  //   const result = await axios("https://type.fit/api/quotes");
+  //   setData(result.data);
+  // });
+
+  // {
+  //   modalIsOpen.map((data) => console.log(data.result)); //check the console and you will get data.
+  // }
+
+  // tu się gubię
+  // useEffect(async () => {
+  //   const result = await axios("https://type.fit/api/quotes");
+  //   setData(result.data);
+  // });
+
+  return (
+    <div className="container center">
+      <h4 className="center">Contact</h4>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.{" "}
+      </p>
+      {/* dodaję Modal */}
+      <button id="quoteButtonOpen" onClick={() => setModalIsOpen(true)}>
+        Check quote
+      </button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        style={{
+          overlay: {
+            backgroundColor: "grey",
+          },
+          content: {
+            color: "orange",
+          },
+        }}
+      >
+        <h1>Quote of the day:</h1>
+        {/* <p>{JSON.stringify(modalIsOpen)}helloł</p> */}
+        <p>{quote} </p>
+        <p>{quoteAuthor}</p>
+        {/* <p>fetchData()</p> */}
+        <div>
+          <button id="quoteButtonClose" onClick={() => setModalIsOpen(false)}>
+            Close
+          </button>
         </div>
-    )
-
-}
+      </Modal>
+    </div>
+  );
+};
 
 export default Contact;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,132 +1,130 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 // Axios jest biblioteką stworzoną na potrzeby komunikacji z API
-import axios from 'axios'
-import {Link} from 'react-router-dom'
-import pokeball from '../pokeball.png'
-import AddPost from './AddPost.js'
+import axios from "axios";
+import { Link } from "react-router-dom";
+import pokeball from "../pokeball.png";
+import AddPost from "./AddPost.js";
 
 // funkcjonalny komponent, bo nie używamy stanów
 
 class Home extends Component {
-    state = {
-        posts: []
-    }
-    //tu używamy Axios, żeby "złapać" dane, zapytania
-    //componentDidMount() to metoda w której możemy wywołać setState() metod, metoda z cyklu życia komponentu!!! Poczytaj gdy komponent się zamontuje, wyświetla 
-    componentDidMount(){
-        //axios działa asynchronicznie, wysyła 'promise', że dana czynność zostanie wykonanana niebawem w przyszłości.zapytanie GET
-        axios.get('https://jsonplaceholder.typicode.com/posts') //pobrać get 
-        //poniższy kod wykona się dopiero wtedy, gdy kod z axios będzie już wykonany. Wykorzystuje resonse obiekt jako parametr 
-            .then(res => {
-                console.log(res)
-                this.setState({
-                    posts: res.data.slice(0,10)
-                })
-            })
-            // obsługa błędu zrób to axios error a\hadling example konsola
-
-    }
-    // metody dodawaj 
-    removePost(id){
-      //tworze zmienną title,  pobieram z tablicy posta z id, na koniec set state od mesage ustaw na tytuł
-      const filteredPost = this.state.posts.filter(kalafior => {
-        return kalafior.id === id;
-      })
-
-      const title = filteredPost[0].title;
-      
-        // filtrujemy "stare" stany, szczypiorek = line newBuyItems
-        const szczypiorek = this.state.posts.filter(marchewka => {
-            //porównujemy item, który został pofiltrowany z itemem, który chcemy usunąć
-            return marchewka.id !== id;
-
-        })
-        
-        // console.log('remove' + id)
-        // console.log(filteredPost[0].title)
-        // console.log(typeof(filteredPost));
-    
+  state = {
+    posts: [],
+  };
+  //tu używamy Axios, żeby "złapać" dane, zapytania
+  //componentDidMount() to metoda w której możemy wywołać setState() metod, metoda z cyklu życia komponentu!!! Poczytaj gdy komponent się zamontuje, wyświetla
+  componentDidMount() {
+    //axios działa asynchronicznie, wysyła 'promise', że dana czynność zostanie wykonanana niebawem w przyszłości.zapytanie GET
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts") //pobrać get
+      //poniższy kod wykona się dopiero wtedy, gdy kod z axios będzie już wykonany. Wykorzystuje resonse obiekt jako parametr
+      .then((res) => {
+        console.log(res);
         this.setState({
-            // robimy kopię starej tablicy posts
-            posts: [...szczypiorek]
-        })
-        this.setState({
-          message: `You have just remove the post`,
-          messageTitle: `${title}`
-        })
-        //after removing all items, all posts:
-        if(szczypiorek.length === 0){
-            this.setState({
-                message: 'You have removed all the posts. No posts on your list'
-            })
-        }
+          posts: res.data.slice(0, 10),
+        });
+      });
+    // obsługa błędu zrób to axios error a\hadling example konsola
+  }
+  // metody dodawaj
+  removePost(id) {
+    //tworze zmienną title,  pobieram z tablicy posta z id, na koniec set state od mesage ustaw na tytuł
+    const filteredPost = this.state.posts.filter((kalafior) => {
+      return kalafior.id === id;
+    });
+
+    const title = filteredPost[0].title;
+
+    // filtrujemy "stare" stany, szczypiorek = line newBuyItems
+    const szczypiorek = this.state.posts.filter((marchewka) => {
+      //porównujemy item, który został pofiltrowany z itemem, który chcemy usunąć
+      return marchewka.id !== id;
+    });
+
+    // console.log('remove' + id)
+    // console.log(filteredPost[0].title)
+    // console.log(typeof(filteredPost));
+
+    this.setState({
+      // robimy kopię starej tablicy posts
+      posts: [...szczypiorek],
+    });
+    this.setState({
+      message: `You have just remove the post`,
+      messageTitle: `${title}`,
+    });
+    //after removing all items, all posts:
+    if (szczypiorek.length === 0) {
+      this.setState({
+        message: "You have removed all the posts. No posts on your list",
+      });
     }
+  }
 
-    //addpost to moja dowlna nazwa?
-    addPost = (addpost) => {
-        addpost.id = Math.floor(Math.random()*100);
-        let bigos = [...this.state.posts, addpost];
-        this.setState({
-            posts: bigos, 
-            // docelowo było message:''
-            message: 'You have added a new post. Gratulation!'
+  //addpost to moja dowlna nazwa?
+  addPost = (addpost) => {
+    addpost.id = Math.floor(Math.random() * 100);
+    let bigos = [...this.state.posts, addpost];
+    this.setState({
+      posts: bigos,
+      // docelowo było message:''
+      message: "You have added a new post. Gratulation!",
+    });
+  };
 
-
-        })
-    }
-
-    //render używamy, żeby zupdatować dane, deskruktuzyzacja 
-    render () {
-        // grabbing the posts property from the state
-        const {posts} = this.state;  // chce wyciągnąc posts ze stanów
-        const postList = posts.length ? 
-        (
-            posts.map(mydelko => {
-                return(  //dla każdego elementu w tablicy posts zwróć to co poniżej, 1n element nadrzędny 
-                    // konsola patrz dlaczego mydelko.id Components i state znajdz
-                    <div className="post card" key={mydelko.id}> 
-                        <img src={pokeball} alt="A pokeball"/>
-                        <div className="card-content">
-                            <div className="right"> 
-                                <button onClick={(e)=> this.removePost(mydelko.id)} id="checkmark">
-                                    <span className="left-line"></span>
-                                    <span className="right-line"></span>
-                                </button>
-                            </div>
-                            <Link to={'/' + mydelko.id}>
-                                <span className="card-title">{mydelko.title}</span>
-                            </Link>
-                            <p>{mydelko.body}</p>
-                        </div>                 
-                    </div>
-                )
-            })
-        ) 
-        :
-        (
-            <div className="center">No posts yet</div>
-            // np /23 strona 404
-        )   
-
+  //render używamy, żeby zupdatować dane, deskruktuzyzacja
+  render() {
+    // grabbing the posts property from the state
+    const { posts } = this.state; // chce wyciągnąc posts ze stanów
+    const postList = posts.length ? (
+      posts.map((mydelko) => {
         return (
-         <div className="container home">
-            <h4 className="center">Home</h4>
-            <table>
-              <tr>
-                  <th className="info center ">Information about posts:</th>
-              </tr>
-              <tr>
-                      <td>
-                      <p className="message left">Message: {this.state.message}</p>
-                      <p className="message-title left">{this.state.messageTitle}</p>
-                      </td>
-              </tr>
-            </table>
-            <AddPost nowanazwa={this.addPost}/>
-            {postList}                
-         </div> 
-        )
-    }
+          //dla każdego elementu w tablicy posts zwróć to co poniżej, 1n element nadrzędny
+          // konsola patrz dlaczego mydelko.id Components i state znajdz
+          <div className="post card" key={mydelko.id}>
+            <img src={pokeball} alt="A pokeball" />
+            <div className="card-content">
+              <div className="right">
+                <button
+                  onClick={(e) => this.removePost(mydelko.id)}
+                  id="checkmark"
+                >
+                  <span className="left-line"></span>
+                  <span className="right-line"></span>
+                </button>
+              </div>
+              <Link to={"/" + mydelko.id}>
+                <span className="card-title">{mydelko.title}</span>
+              </Link>
+              <p>{mydelko.body}</p>
+            </div>
+          </div>
+        );
+      })
+    ) : (
+      <div className="center">No posts yet</div>
+      // np /23 strona 404
+    );
+
+    return (
+      <div className="container home">
+        <h4 className="center">Home</h4>
+        <table>
+          <tr>
+            <th className="info center ">Information about posts:</th>
+          </tr>
+          <tr>
+            <td>
+              <p className="message left">Message: {this.state.message}</p>
+              <p className="message-title left">{this.state.messageTitle}</p>
+            </td>
+          </tr>
+        </table>
+        <AddPost nowanazwa={this.addPost} />
+        {postList}
+      </div>
+    );
+  }
 }
 
 export default Home;
